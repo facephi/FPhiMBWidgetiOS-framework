ac740221bc42dacb197247deb2ee0ea246d462fe
jcmartinezb <jcmartinez@facephi.com>
2022-02-09 11:17:28 +0100

    [SM-349] fix: initial SPM implementation

---
+++ WidgetMBPackage/Tests/WidgetMBPackageTests/WidgetMBPackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import WidgetMBPackage
+
+final class WidgetMBPackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(WidgetMBPackage().text, "Hello, World!")
+    }
+}
