c3c1864f5b4b2815b7368fb49d20aaa6398b6e8d
Jorge Poveda <jpoveda@facephi.com>
2023-03-09 18:17:13 +0100

    [SM-1429] feat: Added Microblink target to the library

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FPhiMBWidgetiOS",
-            targets: ["FPhiMBWidgetiOS"]),
+            targets: ["Microblink"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
