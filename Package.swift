b8f84afb62373ebe7d1b955b41547555105aaa32
Jorge Poveda <jpoveda@facephi.com>
2023-03-13 17:06:24 +0100

    [SM-1429]: feat: Added target to library so the cxxSettings may be configured also in the integrator app

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FPhiMBWidgetiOS",
-            targets: ["Microblink"]),
+            targets: ["FPhiMBWidgetiOS", "Microblink"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
